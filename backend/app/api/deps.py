aaf01e170983dedd82f0a178dd3c78acf272758f
Ricardo Sanjur <ricardosanjurg@gmail.com>
2026-03-04 13:38:07 -0500

    arreglamos lo de deps

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -1,5 +1,20 @@
 from typing import Generator
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt
+from pydantic import ValidationError
+from sqlalchemy.orm import Session
+
+from app.core import security
+from app.core.config import settings
 from app.db.session import SessionLocal
+from app.models.user import User
+from app.schemas.user import TokenPayload
+
+# Make sure this matches the login router URL exactly
+reusable_oauth2 = OAuth2PasswordBearer(
+    tokenUrl="/api/auth/login/access-token"
+)
 
 def get_db() -> Generator:
     try:
@@ -7,3 +22,28 @@ def get_db() -> Generator:
         yield db
     finally:
         db.close()
+
+def get_current_user(
+    db: Session = Depends(get_db), token: str = Depends(reusable_oauth2)
+) -> User:
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[security.ALGORITHM]
+        )
+        token_data = TokenPayload(**payload)
+    except (jwt.JWTError, ValidationError):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Could not validate credentials",
+        )
+    user = db.query(User).filter(User.id == token_data.sub).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+def get_current_active_user(
+    current_user: User = Depends(get_current_user),
+) -> User:
+    if not current_user.is_active:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
