edeeec40d61f1d27169440e4ff59e916d765f99d
Ricardo Sanjur <ricardosanjurg@gmail.com>
2026-03-05 13:19:29 -0500

    arreglado

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,5 +1,4 @@
-from .user import User, UserCreate, UserUpdate, UserInDB
-from .token import Token, TokenPayload
-from .equipment import Equipo, EquipoCreate, EquipoUpdate, Calibracion, CalibracionCreate, CalibracionUpdate
+from .user import User, UserCreate, UserUpdate, Token, TokenPayload
+from .equipment import Equipo, EquipoCreate, EquipoUpdate, Calibracion, CalibracionCreate
 from .quotation import Quotation, QuotationCreate, QuotationUpdate, QuotationItem, QuotationItemCreate, QuotationItemUpdate
 from .service_order import ServiceOrder, ServiceOrderCreate, ServiceOrderUpdate
